0499e05d1a1a92bd9fabde809ac2ef9c50600527
xovest <79008917+xovest@users.noreply.github.com>
2021-10-17 21:16:45 +0300

    already commited

---
+++ script.js
@@ -0,0 +1,60 @@
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+  const arrayValue = quoteInputElement.value.split('');
+  let correct = true;
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character == null) {
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.remove('incorrect');
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct');
+      characterSpan.classList.remove('incorrect');
+    } else {
+      characterSpan.classList.add('incorrect');
+      characterSpan.classList.remove('correct');
+      correct = false;
+    }
+  });
+
+  if (correct) renderNewQuote();
+});
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content);
+}
+
+async function renderNewQuote() {
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerText = '';
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = null;
+  startTimer();
+}
+
+let startTime;
+function startTimer() {
+  timerElement.innerText = 0;
+  startTimer = new Date();
+  setInterval(() => {
+    timer.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
+}
+
+renderNewQuote();
